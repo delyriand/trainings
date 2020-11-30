0dca5f99c21eb8841de2151f6ee67bf4802226bf
Maxime LECLERCQ <maxime.leclercq@smile.fr>
2020-11-30 15:26:46 +0100

    Optimize data provider

--- src/Training/Bundle/FirstThemeBundle/Layout/DataProvider/WeatherInformation.php
+++ src/Training/Bundle/FirstThemeBundle/Layout/DataProvider/WeatherInformation.php
@@ -2,8 +2,16 @@
 
 namespace Training\Bundle\FirstThemeBundle\Layout\DataProvider;
 
+use Guzzle\Http\Client;
+
 class WeatherInformation
 {
+    const WEATHER_API_URL_PATTERN = 'https://api.openweathermap.org/data/2.5/weather?q=%s&appid=%s';
+
+    private $apiKey = 'XXXX';
+
+    private $city = 'Mouvaux,fr';
+
     /**
      * Return current weather.
      *
@@ -11,6 +19,29 @@ class WeatherInformation
      */
     public function getCurrentWeather(): string
     {
-        return 'fog';
+        $client = new Client();
+        $request = $client->createRequest(
+            'GET',
+            "http://ipinfo.io/json"
+        );
+        $response = $request->send();
+        $json = $response->json();
+
+        $city = $this->city;
+        if ($json) {
+            $city = $json['city'].','.$json['country'];
+        }
+
+        $request = $client->createRequest(
+            'GET',
+            sprintf(self::WEATHER_API_URL_PATTERN, $city, $this->apiKey)
+        );
+        $response = $request->send();
+        $json = $response->json();
+        if ($json) {
+            return $json['weather'][0]['description'];
+        }
+
+        return 'i don\'t know';
     }
 }
